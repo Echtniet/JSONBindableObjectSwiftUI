5103a2f6056779dfbe32676502a6d7a4582f6198
Clinton Davelaar <davelaarprogramming@gmail.com>
2021-02-04 12:28:29 -0600

    Initial Commit

---
+++ JSONBindableObjectSwiftUI/JSONBindableObjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONBindableObjectSwiftUIApp.swift
+//  JSONBindableObjectSwiftUI
+//
+//  Created by student on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct JSONBindableObjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
