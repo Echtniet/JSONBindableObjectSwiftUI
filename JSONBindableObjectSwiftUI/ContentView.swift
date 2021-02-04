1a4c19624defa90184f0cb642409f9374e7f02bb
Clinton Davelaar <davelaarprogramming@gmail.com>
2021-02-04 12:58:23 -0600

    Start retrieval JSON data

--- JSONBindableObjectSwiftUI/ContentView.swift
+++ JSONBindableObjectSwiftUI/ContentView.swift
@@ -6,11 +6,34 @@
 //
 
 import SwiftUI
+import Combine
+
+struct Course: Decodable, Identifiable, Hashable {
+    let id: Int
+    let name, imageUrl: String
+}
+
+class NetworkManager: ObservableObject {
+    var didChange = PassthroughSubject <NetworkManager, Never>()
+    
+    var courses = [Course](){
+        didSet{
+            didChange.send(self)
+        }
+    }
+}
 
 struct ContentView: View {
+    
+    let courses: [Course] = [.init(id: 0, name: "No Name", imageUrl: "no url"),
+                             .init(id: 1, name: "No no Name", imageUrl: "no url")]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List (courses){ course in
+                Text(course.name)
+            }.navigationBarTitle(Text("Courses"))
+        }
     }
 }
 
